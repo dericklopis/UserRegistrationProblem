9425d15bd9a2034b85eae9542d8e113e6d5f4768
dericklopis <stanforever08@gmail.com>
2023-01-05 17:59:05 +0530

    Added: First word

--- UserRegistrationProgram/Program.cs
+++ UserRegistrationProgram/Program.cs
@@ -10,6 +10,21 @@ namespace UserRegistrationProgram
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome To Regex User Registration Problem");
+            Console.ReadLine();
+            RegexClass code = new RegexClass();
+            Console.WriteLine("1 - Enter First Name");
+            Console.WriteLine("2 - Enter Last Name");
+            int select = Convert.ToInt32(Console.ReadLine());
+            switch (select)
+            {
+                case 1:
+                    Console.WriteLine("Enter First Name");
+                    Console.ReadLine();
+                    string firstName = Console.ReadLine();
+                    code.name(firstName);
+                    break;
+            }
         }
     }
 }
---
+++ UserRegistrationProgram/RegexClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProgram
+{
+    internal class RegexClass
+    {
+        public void name(string name)
+        {
+            const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+
+            if (Regex.IsMatch(name, NAME))
+            {
+                Console.WriteLine(name + " is a valid Name");
+                return;
+            }
+            Console.WriteLine(name + " is a invalid Name");
+            Console.ReadLine();
+            return;
+        }
+    }
+}
