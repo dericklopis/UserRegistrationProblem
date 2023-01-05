2e4f8660c659e0869aa8410b03a32ffc9555f99e
dericklopis <stanforever08@gmail.com>
2023-01-05 18:16:05 +0530

    Added: Phone number

--- UserRegistrationProgram/RegexClass.cs
+++ UserRegistrationProgram/RegexClass.cs
@@ -26,12 +26,25 @@ namespace UserRegistrationProgram
             Console.WriteLine(result);
             Console.ReadLine();
         }
+        //UC3
+        //E-Mail validation
         public const string EMAIL_REGEX = "^[a-z0-9]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}$";
         public void ValidationEmail(string email)
         {
             Regex regex = new Regex(EMAIL_REGEX);
             bool result = regex.IsMatch(email);
             Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        //UC4
+        // Pre define Mobile Format Validation
+        public const string MOBILENUMBER_REGEX = "^[0-9]+[\\s]+[0-9]{10}$";
+        public void ValidationPhoneNumber(string phonenumber)
+        {
+            Regex regex = new Regex(MOBILENUMBER_REGEX);
+            bool result = regex.IsMatch(phonenumber);
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
